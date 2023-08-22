58e96513066f90c6e6d285b767223b74cf4e2531
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-08-21 20:57:06 -0500

    Create function mergeSportCenter

--- packages/database/src/schema/utils.ts
+++ packages/database/src/schema/utils.ts
@@ -1,9 +1,22 @@
-import { User } from "../db/Entities";
+import { SportCenter, User } from "../db/Entities";
+import { FireStoreSportCenter } from "../db/Firebase/Firestore/SportCenter";
 import { FireStoreUser } from "../db/Firebase/Firestore/User";
 
 export const mergeUsers = (sqlUser: User[], firestoreUser: FireStoreUser[]) => {
   return sqlUser.map((obj1) => {
     const obj2 = firestoreUser.find((obj2) => obj1.userId === obj2.userId);
+    return { ...obj1, ...obj2 };
+  });
+};
+
+export const mergeSportCenter = (
+  sqlSportCenter: SportCenter[],
+  firestoreSPortCenter: FireStoreSportCenter[]
+) => {
+  return sqlSportCenter.map((obj1) => {
+    const obj2 = firestoreSPortCenter.find(
+      (obj2) => obj1.sportCenterId === obj2.sportCenterId
+    );
     return { ...obj1, ...obj2 };
   });
 };
