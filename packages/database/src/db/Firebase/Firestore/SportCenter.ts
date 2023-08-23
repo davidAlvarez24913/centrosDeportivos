3803a0fcbddbd37e9084c27a896e40998eed5a71
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-08-22 22:43:37 -0500

    Create CRUD operantions for SportCenter

---
+++ packages/database/src/db/Firebase/Firestore/SportCenter.ts
@@ -0,0 +1,37 @@
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore/lite";
+import { db } from "../config";
+
+export type FireStoreSportCenter = {
+  sportCenterId: number;
+  images?: string[];
+  calification?: number;
+};
+const document = "sportscenter";
+
+export const listSportCenters = async () => {
+  const sportCenterSnapShot = await getDocs(collection(db, document));
+  return sportCenterSnapShot.docs.map((sc) => sc.data());
+};
+export const findSportCenter = async (aportCenerId: string) => {
+  const docRef = doc(db, document, aportCenerId);
+  const docSnap = await getDoc(docRef);
+  return docSnap.data();
+};
+export const createFirestoreSportCenter = async (sc: FireStoreSportCenter) =>
+  await setDoc(doc(db, document, `${sc.sportCenterId}`), sc);
+
+export const updateFirestoreSportCenter = async (sc: FireStoreSportCenter) => {
+  const docRef = doc(db, document, `${sc.sportCenterId}`);
+  await updateDoc(docRef, sc);
+};
+
+export const deleteFirestoreSportCEnter = async (sportCenterId: string) =>
+  await deleteDoc(doc(db, document, sportCenterId));
