6026ac4f7786251afa5184db7288e0d474632b38
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-08-22 22:49:21 -0500

    Add resolver SportCenter

--- packages/database/src/schema/SportCenter/sportCenterResolvers.ts
+++ packages/database/src/schema/SportCenter/sportCenterResolvers.ts
@@ -1,11 +1,27 @@
 import { SportCenter } from "../../db/Entities";
+import { GraphQLError } from "graphql";
+import {
+  FireStoreSportCenter,
+  createFirestoreSportCenter,
+  findSportCenter,
+  listSportCenters,
+  updateFirestoreSportCenter,
+} from "../../db/Firebase/Firestore/SportCenter";
+import { mergeSportCenter } from "../utils";
+
 export const sportCenterResolvers = {
   Query: {
     listSportCenters: async () => {
+      const firestoreSportCenter =
+        (await listSportCenters()) as FireStoreSportCenter[];
       const sportsCenters = await SportCenter.find({
         relations: { services: true },
       });
-      const result = sportsCenters.map((sportCenter) => {
+      const mergedSportsCenter = mergeSportCenter(
+        sportsCenters,
+        firestoreSportCenter
+      );
+      const result = mergedSportsCenter.map((sportCenter) => {
         return {
           ...sportCenter,
           services: sportCenter.services.map((service) => {
@@ -18,8 +34,62 @@ export const sportCenterResolvers = {
   },
   Mutation: {
     createSportCenter: async (root: any, { input }: any) => {
-      const result = await SportCenter.insert(input);
-      return { ...input, id: result.identifiers[0].id };
+      const searchedByName = await SportCenter.find({
+        where: { name: input.name },
+      });
+      // Validation to not create with same name
+      if (searchedByName.length === 0) {
+        const result = await SportCenter.insert(input);
+        const sportCenterId = result.identifiers[0].sportCenterId;
+        await createFirestoreSportCenter({
+          sportCenterId: sportCenterId,
+          images: input.images,
+          calification: input.calification ?? 0,
+        });
+        return { ...input, id: result.identifiers[0].id };
+      } else {
+        throw new GraphQLError("Nombre del centro deportivo ya existe", {
+          extensions: {
+            code: "SPORT_CENTER_ALREADY_EXISTS",
+            argumentName: "name",
+          },
+        });
+      }
+    },
+    updateSportCenter: async (root: any, { input }: any) => {
+      // query to get current data, if dont pass sql data not update record
+      const currentSportCenterSQL = await SportCenter.find({
+        where: { sportCenterId: input.sportCenterId },
+      });
+      const currentSportCenterNoSQL = await findSportCenter(
+        input.sportCenterId
+      );
+
+      try {
+        await updateFirestoreSportCenter({
+          sportCenterId: input.sportCenterId,
+          images: input.images ?? currentSportCenterNoSQL?.images,
+          calification:
+            input.calification ?? currentSportCenterNoSQL?.calification,
+        });
+        await SportCenter.update(
+          {
+            sportCenterId: input.sportCenterId,
+          },
+          {
+            name: input.name ?? currentSportCenterSQL[0].name,
+            phone: input.phone ?? currentSportCenterSQL[0].phone,
+            ubication: input.ubication ?? currentSportCenterSQL[0].ubication,
+          }
+        );
+        return {
+          status: "Ok",
+          message: "Centro deportivo se actualizo exitosamente",
+        };
+      } catch (error) {
+        console.log(error);
+        return { status: "Failed", message: "No se pudo actualizar" };
+      }
     },
   },
 };
