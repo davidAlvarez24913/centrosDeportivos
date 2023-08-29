567e8e8665afdc124e8b728749739039905fed99
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-08-29 14:40:10 -0500

    Add config tailwind

---
+++ app/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [
+      // The directory of your Ionic application
+      './src/**/*.{html,js,ts}',
+    ],
+    darkMode: false,
+    theme: {
+      extend: {
+        colors: {
+          primary: '#007AFF',
+          secondary: '#29B6F6',
+        },
+      },
+    },
+  };
