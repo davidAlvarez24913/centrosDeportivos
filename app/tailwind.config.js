885fe7670c301b548e0fbe22f5a91c50c755bf41
MiguelCaraguay <miguelcaraguay.1502@gmail.com>
2023-08-30 11:09:26 -0500

    add custom tailwind styles

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-    purge: [
-      // The directory of your Ionic application
-      './src/**/*.{html,js,ts}',
-    ],
-    darkMode: false,
-    theme: {
-      extend: {
-        colors: {
-          primary: '#98CE00',
-          background: '#12111F',
-        },
+  purge: [
+    // The directory of your Ionic application
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: "#98CE00",
+        background: "#12111F",
+        customText: "#F9FAFB",
       },
     },
-  };
+  },
+};
