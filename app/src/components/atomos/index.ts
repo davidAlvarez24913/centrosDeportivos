bdb16379d828bb1114f5b7706017d637f91a4245
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-08-30 19:36:08 -0500

    Create PhotoPicker

--- app/src/components/atomos/index.ts
+++ app/src/components/atomos/index.ts
@@ -1,3 +1,4 @@
 export { default as Background } from "./Background";
 export { default as Header } from "./Header";
 export { default as MainCard } from "./MainCard";
+export { default as PhotoPicker } from "./PhotoPicker";
