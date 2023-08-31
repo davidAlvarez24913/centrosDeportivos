c9f21589ba3825f9a9ca76c4bbcc03e81c62b990
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-08-31 08:24:57 -0500

    Clena Photopicker

--- app/src/components/atomos/PhotoPicker.tsx
+++ app/src/components/atomos/PhotoPicker.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Camera, CameraResultType } from "@capacitor/camera";
 
 type PropsPhotPicker = {
-  // Remenber this props will be ook useSate
+  // Remenber this props will be hook useSate
   photo: string | undefined;
   setPhoto: (img: string | undefined) => void;
 };
