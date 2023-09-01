c45ca73cca988a53d8e1a8d8d9de051a3fa59aeb
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-08-31 23:27:39 -0500

    Add StarRank

--- app/src/components/moleculas/index.ts
+++ app/src/components/moleculas/index.ts
@@ -5,3 +5,4 @@ export { default as ReservationCard } from "./ReservationCard";
 export { default as Modal } from "./Modal";
 export { default as Accordion } from "./Accordion";
 export { default as Segment } from "./Segment";
+export { default as StarRank } from "./StarRank";
