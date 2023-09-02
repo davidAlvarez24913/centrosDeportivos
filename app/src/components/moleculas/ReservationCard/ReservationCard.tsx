46798759b7b60e8263b38f13eea39a9b01314c04
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-09-02 08:42:09 -0500

    Update ReservationCard

--- app/src/components/moleculas/ReservationCard/ReservationCard.tsx
+++ app/src/components/moleculas/ReservationCard/ReservationCard.tsx
@@ -2,30 +2,30 @@ import React from "react";
 import { MainCard } from "src/components/atomos";
 
 type ReservationProps = {
-  title: string;
-  service: string;
-  schedule: string;
+  nameSportCenter: string;
+  nameService: string;
+  rangeHour: string;
   paymentStatus: boolean;
 };
 const ReservationCard = ({
-  title,
-  service,
-  schedule,
+  nameSportCenter,
+  nameService,
+  rangeHour,
   paymentStatus,
 }: ReservationProps) => {
   return (
     <MainCard>
       <div className="flex flex-row gap-6 justify-between cursor-pointer px-4 py-2">
         <div className="flex flex-col w-2/3">
-          <h2 className="text-xl font-bold">{title}</h2>
-          <h3 className="text-base font-medium leading-3">{service}</h3>
+          <h2 className="text-xl font-bold">{nameSportCenter}</h2>
+          <h3 className="text-base font-medium leading-3">{nameService}</h3>
           <div className="flex flex-row items-center gap-2 pt-2">
             <img
               src="assets/icon/schedule.svg"
               alt="sportIcon"
               className="w-5 rounded-r-2xl"
             />
-            <p className="font-light text-xs leading-4 ">{schedule}</p>
+            <p className="font-light text-xs leading-4 ">{rangeHour}</p>
           </div>
         </div>
         <div className="w-1/3">
