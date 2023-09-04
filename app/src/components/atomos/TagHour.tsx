eb572e4f5f32a8d29a5fdcdda0ce80f29559bd1b
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-09-04 18:51:39 -0500

    Add prop onClick

--- app/src/components/atomos/TagHour.tsx
+++ app/src/components/atomos/TagHour.tsx
@@ -3,8 +3,9 @@ import React from "react";
 type PropsTagHour = {
   rangeHour: string;
   available: boolean;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
 };
-const TagHour = ({ rangeHour, available }: PropsTagHour) => {
+const TagHour = ({ rangeHour, available, onClick }: PropsTagHour) => {
   const styles = available
     ? " w-14 text-center bg-primary text-background"
     : " w-14 text-center bg-background text-customText";
@@ -14,6 +15,7 @@ const TagHour = ({ rangeHour, available }: PropsTagHour) => {
         "flex flex-col items-center w-min h-7 p-2 text-[8px] gap-1 rounded-2xl border-2 border-primary" +
         styles
       }
+      onClick={onClick}
     >
       <h4 className={styles}>{rangeHour}</h4>
     </div>
