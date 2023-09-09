aa186b7f2caef519b909537ce54a205fdb58e770
MiguelCaraguay <miguelcaraguay.1502@gmail.com>
2023-09-09 14:22:26 -0500

    fix margin in profile page

--- dashboard/src/pages/Profile.tsx
+++ dashboard/src/pages/Profile.tsx
@@ -4,9 +4,9 @@ import { accounts } from "../data";
 import AccountSection from "../components/organismos/AccountSection";
 const ProfilePage = () => {
   return (
-    <div className="flex flex-row gap-20">
+    <div className="flex flex-row justify-between">
       <Navbar nameSportCenter="La Pampita" />
-      <div className="flex flex-row mt-20 px-10 justify-between w-full">
+      <div className="flex flex-row w-4/5 mt-20 mx-10 justify-between">
         <div className="w-2/3">Aqui poner contenido de perfil xd</div>
         <div className="w-1/3">
           <AccountSection accounts={accounts} />
