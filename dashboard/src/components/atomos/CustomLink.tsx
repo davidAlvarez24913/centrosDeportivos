c568de25acdd5ab8b620f13e17a020b1a68957bc
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-09-09 07:51:09 -0500

    Add customlink

---
+++ dashboard/src/components/atomos/CustomLink.tsx
@@ -0,0 +1,45 @@
+type PropsCustomLink = {
+  label: string;
+  srcImage: string;
+  width: string | number;
+  height: string | number;
+  isActive: boolean;
+} & React.ComponentProps<"a">;
+const CustomLink = ({
+  label,
+  srcImage,
+  width,
+  height,
+  isActive,
+  ...rest
+}: PropsCustomLink) => {
+  const styleByRoute = isActive
+    ? "text-background bg-primary"
+    : "text-customText";
+  return (
+    <a
+      className={
+        "flex rounded-xl gap-5 items-center px-3 py-2  no-underline left-0" +
+        styleByRoute
+      }
+      {...rest}
+    >
+      <img
+        src={srcImage}
+        alt={label}
+        width={width}
+        height={height}
+        className={styleByRoute}
+      />
+      <p
+        className={`${
+          isActive ? "text-background" : "text-customText"
+        } font-normal text-2xl`}
+      >
+        {label}
+      </p>
+    </a>
+  );
+};
+
+export default CustomLink;
