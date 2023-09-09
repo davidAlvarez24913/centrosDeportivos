cc9cf8a4426ff47049e44204a6a7472bc2c67246
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-09-09 09:25:26 -0500

    Add CustomInput

---
+++ dashboard/src/components/atomos/CustomInput.tsx
@@ -0,0 +1,58 @@
+type PropsCustomInput = {
+  label?: string;
+  color: "white" | "blue";
+  errorMessage?: string;
+  onClickIcon?: () => void;
+  pathSVG?: string;
+} & React.ComponentProps<"input">;
+
+const CustomInput = ({
+  label,
+  color,
+  pathSVG,
+  errorMessage,
+  onClickIcon,
+  ...rest
+}: PropsCustomInput) => {
+  const auxIcon = "/icons/eye-outline.svg";
+  const flagIcon = !(rest.type === "password") && pathSVG !== undefined;
+  const onClick = () => {};
+
+  const styleBlue = color === "blue" && " border-background";
+  const styleWhite = color === "white" && " border-customText";
+  const placeholderBlue =
+    color === "blue" && "text-background placeholder-background font-light";
+  const placeholderWhite =
+    color === "white" && "text-customText placeholder-customText font-light";
+  return (
+    <div className="flex flex-col w-full">
+      {label && (
+        <label
+          className={`${styleBlue}  ${styleWhite}`}
+          htmlFor={label?.replaceAll(" ", "_")}
+        >
+          {label}
+        </label>
+      )}
+      <div
+        className={`${styleBlue}  ${styleWhite} flex w-full rounded-2xl border-2 py-1 px-3`}
+      >
+        <input
+          className={`bg-transparent outline-none ${styleBlue}  ${styleWhite} ${placeholderBlue}  ${placeholderWhite}  `}
+          {...rest}
+          name={label?.replaceAll(" ", "_")}
+          placeholder={rest.placeholder}
+        />
+        {flagIcon && <img src={pathSVG} alt={pathSVG} />}
+        {rest.type === "password" && (
+          <img src={auxIcon} alt={auxIcon} onClick={onClickIcon ?? onClick} />
+        )}
+      </div>
+      {errorMessage && (
+        <p className="text-red-400 text-xs px-4">{errorMessage as string}</p>
+      )}
+    </div>
+  );
+};
+
+export default CustomInput;
