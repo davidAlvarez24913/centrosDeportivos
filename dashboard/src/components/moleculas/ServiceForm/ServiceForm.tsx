534d97ba83ca9e6e21968a4cb117f64651c05b8b
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-09-20 18:34:10 -0500

    Update ServiceForm

--- dashboard/src/components/moleculas/ServiceForm/ServiceForm.tsx
+++ dashboard/src/components/moleculas/ServiceForm/ServiceForm.tsx
@@ -7,12 +7,21 @@ import {
 } from "../../atomos";
 import { Sport } from "schema";
 import ImageInput from "../ImageInput";
+import useUser from "../../../Hooks/useUser";
 type ServiceFormProps = {
   onSubmit: () => void;
 };
 const sports = Object.values(Sport);
 const ServiceForm = ({ onSubmit }: ServiceFormProps) => {
+  const { user } = useUser();
   const [FileBlob, setFileBlob] = useState<string>();
+  const [newService, setNewService] = useState({
+    name: "",
+    sport: "",
+    description: "",
+    sportCenterId: user?.uid,
+    image: "name image of bucket",
+  });
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     onSubmit();
@@ -29,15 +38,37 @@ const ServiceForm = ({ onSubmit }: ServiceFormProps) => {
         name="name"
         label="Nombre del Servicio"
         placeholder="Nombre del Servicio"
+        onChange={(e) =>
+          setNewService({ ...newService, name: e.currentTarget.value })
+        }
       />
-      <CustomTextarea color="blue" label="Descripción" />
-      <CustomSelect color="blue" sports={sports} name="sport" label="Deporte" />
+      <CustomTextarea
+        color="blue"
+        label="Descripción"
+        onChange={(e) => {
+          setNewService({ ...newService, description: e.currentTarget.value });
+        }}
+      />
+      <CustomSelect
+        color="blue"
+        sports={sports}
+        name="sport"
+        label="Deporte"
+        onChange={(e) =>
+          setNewService({ ...newService, sport: e.currentTarget.value })
+        }
+      />
       <ImageInput
         label="agregar imagen del servicio"
         fileBlob={FileBlob}
         setFileBlob={setFileBlob}
       ></ImageInput>
-      <CustomButton title="crear servicio" color="sucessfull" type="submit" />
+      <CustomButton
+        title="crear servicio"
+        color="sucessfull"
+        type="submit"
+        onClick={() => console.log(newService)}
+      />
     </form>
   );
 };
