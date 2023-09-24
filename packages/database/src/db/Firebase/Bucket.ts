56b639b653f1aa9a903425f8afe08d06026e483b
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-09-23 23:39:57 -0500

    Add bucket

---
+++ packages/database/src/db/Firebase/Bucket.ts
@@ -0,0 +1,39 @@
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  uploadBytesResumable,
+  uploadString,
+} from "firebase/storage";
+import { storage } from "./config";
+
+function dataURLToFile(dataURL: string, fileName: string): File | null {
+  try {
+    const blob = dataURL.startsWith("data:")
+      ? new Blob([dataURL], { type: "" })
+      : null;
+
+    if (!blob) {
+      return null;
+    }
+
+    return new File([blob], fileName);
+  } catch (error) {
+    console.error("Error al convertir Data URL a File:", error);
+    return null;
+  }
+}
+
+export const uploadFile = async (file: string, nameImage: string) => {
+  const storageRef = ref(storage, nameImage);
+  await uploadString(storageRef, file, "data_url").then((snapshot) => {
+    console.log("Uploaded a image file!", snapshot);
+  });
+  const fileName = "imagen.png"; // Reemplaza con el nombre deseado para el archivo
+  // const fileAux = dataURLToFile(file, fileName);
+  // const uploadTask = uploadBytesResumable(storageRef, fileAux!);
+  const url = await getDownloadURL(storageRef);
+  // const loading = (await uploadTask).state;
+  // return { loading: loading, imgaeUrl: url };
+  return url;
+};
