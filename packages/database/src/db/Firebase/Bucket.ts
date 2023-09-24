3054545b01e35094eab29f28331d7ab8a0c8c574
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-09-23 23:49:50 -0500

    Clean code

--- packages/database/src/db/Firebase/Bucket.ts
+++ packages/database/src/db/Firebase/Bucket.ts
@@ -19,7 +19,7 @@ function dataURLToFile(dataURL: string, fileName: string): File | null {
 
     return new File([blob], fileName);
   } catch (error) {
-    console.error("Error al convertir Data URL a File:", error);
+    // console.error("Error al convertir Data URL a File:", error);
     return null;
   }
 }
@@ -27,13 +27,9 @@ function dataURLToFile(dataURL: string, fileName: string): File | null {
 export const uploadFile = async (file: string, nameImage: string) => {
   const storageRef = ref(storage, nameImage);
   await uploadString(storageRef, file, "data_url").then((snapshot) => {
-    console.log("Uploaded a image file!", snapshot);
+    // console.log("Uploaded a image file!", snapshot);
   });
   const fileName = "imagen.png"; // Reemplaza con el nombre deseado para el archivo
-  // const fileAux = dataURLToFile(file, fileName);
-  // const uploadTask = uploadBytesResumable(storageRef, fileAux!);
   const url = await getDownloadURL(storageRef);
-  // const loading = (await uploadTask).state;
-  // return { loading: loading, imgaeUrl: url };
   return url;
 };
