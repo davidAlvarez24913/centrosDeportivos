ecc09e6fe8e5464657d6e61976e457617acf38bd
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-09-24 19:43:55 -0500

    Update modal

--- dashboard/src/components/moleculas/Modal/Modal.tsx
+++ dashboard/src/components/moleculas/Modal/Modal.tsx
@@ -1,10 +1,14 @@
 import React, { PropsWithChildren } from "react";
+import { MoreOptions } from "../../atomos";
 
 type ModalPorps = {
   title: string;
   modalState: boolean;
   closeModal: () => void;
   cantClose?: boolean;
+  onEdit?: () => void;
+  onDelete?: () => void;
+  moreOptions?: boolean;
 } & PropsWithChildren;
 
 const Modal = ({
@@ -13,6 +17,9 @@ const Modal = ({
   closeModal,
   cantClose,
   children,
+  onEdit,
+  onDelete,
+  moreOptions = false,
 }: ModalPorps) => {
   return (
     <>
@@ -21,21 +28,26 @@ const Modal = ({
           <div className="bg-white flex flex-col w-full max-w-lg max-h-[95%] border overflow-y-auto border-white rounded-xl px-7 py-8 ">
             <div className="text-base flex flex-row justify-between">
               <h2 className="text-xl font-bold">{title}</h2>
-              <button
-                type="button"
-                className="flex items-center justify-start"
-                onClick={() => {
-                  closeModal();
-                }}
-              >
-                {!cantClose && (
-                  <img
-                    src="/icons/close.svg"
-                    alt="close button"
-                    className="w-6"
-                  />
+              <div className="flex">
+                {moreOptions && (
+                  <MoreOptions onDelete={onDelete!} onEdit={onEdit!} />
                 )}
-              </button>
+                <button
+                  type="button"
+                  className="flex items-center justify-start"
+                  onClick={() => {
+                    closeModal();
+                  }}
+                >
+                  {!cantClose && (
+                    <img
+                      src="/icons/close.svg"
+                      alt="close button"
+                      className="w-6"
+                    />
+                  )}
+                </button>
+              </div>
             </div>
             {children}
           </div>
