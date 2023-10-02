78ef87ec5a826dd09b172ac70495c1da518b3f70
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-10-02 15:32:39 -0500

    Fix images

--- packages/database/src/ResolversModules/Service/UpdateServiceResolver.ts
+++ packages/database/src/ResolversModules/Service/UpdateServiceResolver.ts
@@ -11,14 +11,52 @@ const UpdateServiceResolver = async (root: any, { input }: any) => {
       serviceId: input.serviceId,
     });
     const currentServiceNoSQL = await findService(input.serviceId);
-
     const auxNameImage =
       "services/" + input.sportCenterId + "-" + currentServiceSQL?.serviceId;
 
+    const currenImage = auxNameImage + "#" + currentServiceNoSQL?.image;
+
     let imageUrl = "";
-    // compare that images are different
-    if (input.image != currentServiceNoSQL?.image) {
-      if (input.image !== "") {
+    if (input.image === "") {
+      // save on firestore and delete image bucket
+      currentServiceNoSQL?.image && (await deleteImage(auxNameImage));
+      await updateFirestoreService({
+        serviceId: input.serviceId,
+        image: input.image,
+        disponibility: { ...(input.disponibility as Disponibility) },
+      });
+      await Service.update(
+        {
+          serviceId: input.serviceId,
+        },
+        {
+          name: input.name,
+          description: input.description,
+          sport: input.sport,
+        }
+      );
+    } else {
+      const imageAux = auxNameImage + "#" + input.image;
+
+      if (imageAux === currenImage) {
+        // update other fields, image bucke no change
+        await updateFirestoreService({
+          serviceId: input.serviceId,
+          image: auxNameImage + "#" + input.image,
+          disponibility: { ...(input.disponibility as Disponibility) },
+        });
+        await Service.update(
+          {
+            serviceId: input.serviceId,
+          },
+          {
+            name: input.name,
+            description: input.description,
+            sport: input.sport,
+          }
+        );
+      } else {
+        //create image and save
         imageUrl = await uploadFile(input.image, auxNameImage);
         if (imageUrl) {
           await updateFirestoreService({
@@ -37,40 +75,7 @@ const UpdateServiceResolver = async (root: any, { input }: any) => {
             }
           );
         }
-      } else {
-        await deleteImage(auxNameImage);
-        await updateFirestoreService({
-          serviceId: input.serviceId,
-          image: input.image,
-          disponibility: { ...(input.disponibility as Disponibility) },
-        });
-        await Service.update(
-          {
-            serviceId: input.serviceId,
-          },
-          {
-            name: input.name,
-            description: input.description,
-            sport: input.sport,
-          }
-        );
       }
-    } else {
-      await updateFirestoreService({
-        serviceId: input.serviceId,
-        image: input.image,
-        disponibility: { ...(input.disponibility as Disponibility) },
-      });
-      await Service.update(
-        {
-          serviceId: input.serviceId,
-        },
-        {
-          name: input.name,
-          description: input.description,
-          sport: input.sport,
-        }
-      );
     }
 
     return {
