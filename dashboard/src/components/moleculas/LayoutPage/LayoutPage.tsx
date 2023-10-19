1669cf1e90c587fddc4e965260f0bbd1db22e93a
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-10-19 15:16:38 -0500

    fix default

--- dashboard/src/components/moleculas/LayoutPage/LayoutPage.tsx
+++ dashboard/src/components/moleculas/LayoutPage/LayoutPage.tsx
@@ -7,7 +7,7 @@ type PropsLayout = {
 const LayoutPage = ({ children, nameSportCenter }: PropsLayout) => {
   return (
     <div className="flex flex-row gap-5 md:gap-20">
-      <Navbar nameSportCenter={nameSportCenter ?? "La Pampita"} />
+      <Navbar nameSportCenter={nameSportCenter ?? "Centro deportivo "} />
       <div className="flex flex-col w-2/3 mt-20 px-4 ">{children}</div>
     </div>
   );
