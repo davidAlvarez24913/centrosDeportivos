525934e9d1bc5603d615afa0b4c600e05c811ef1
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-10-21 09:12:39 -0500

    Update envs

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -2,9 +2,6 @@ import { dataSource } from "./db/TypeOrm/config";
 import { app } from "./db/Firebase/config";
 import { apolloServer } from "./app";
 import "reflect-metadata";
-// import ip from "ip";
-
-// const ip_local = ip.address();
 
 dataSource
   .initialize()
@@ -12,7 +9,7 @@ dataSource
   .catch((error) => console.log(error));
 
 apolloServer
-  .listen({ host: "64.227.3.57", port: 4000 })
+  .listen({ host: process.env.IP_ADDRESS, port: 4000 })
   .then(({ url }) => {
     console.log(`Server ready alt ${url}graphql`);
   });
--- packages/schema/src/gql/apolloClient.ts
+++ packages/schema/src/gql/apolloClient.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-// this uri will be enviroment variable
-
-const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+const httpLink = new HttpLink({
+  uri: `http://${process.env.IP_ADDRESS}:4000/graphql`,
+});
 export const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
