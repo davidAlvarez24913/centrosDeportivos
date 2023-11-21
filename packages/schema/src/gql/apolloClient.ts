71928d0e8da599b143a6aae38f6bcd0572cabf94
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-11-20 21:31:32 -0500

    Comment ip_production

--- packages/schema/src/gql/apolloClient.ts
+++ packages/schema/src/gql/apolloClient.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-const httpLink = new HttpLink({ uri: "http://159.89.85.206:4000/graphql" });
+// const httpLink = new HttpLink({ uri: "http://159.89.85.206:4000/graphql" });
+const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
 export const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
