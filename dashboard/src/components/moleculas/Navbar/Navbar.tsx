4da08df47dae58802a0cd47810700b34947a4026
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-11-28 15:48:44 -0500

    Control navbar links by role

--- dashboard/src/components/moleculas/Navbar/Navbar.tsx
+++ dashboard/src/components/moleculas/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import { LinkLogo, LogoutButton, MenuBar } from "../../atomos";
 import { memo, useEffect, useState } from "react";
 import LinkNav from "../LinkNav";
-import { useGetPermissionSportCenterQuery } from "schema";
 import useUser from "../../../Hooks/useUser";
+import { useIsAdminQuery } from "schema";
 
 type PropsNavbar = {
   nameSportCenter: string;
@@ -11,14 +11,15 @@ const Navbar = ({ nameSportCenter }: PropsNavbar) => {
   const { user, loadingUser } = useUser();
   const [isOpen, setIsOpen] = useState(false);
   const [width, setWidth] = useState(0);
-  const { data, loading } = useGetPermissionSportCenterQuery({
-    variables: { sportCenterId: user?.uid as string },
+  const { data: admin, loading: loadingAdmin } = useIsAdminQuery({
+    variables: { adminId: user?.uid as string },
   });
+
   const flag =
     !loadingUser &&
-    !loading &&
-    data?.getSportCenter?.superUser !== null &&
-    data?.getSportCenter?.superUser;
+    !loadingAdmin &&
+    admin?.isAdmin !== null &&
+    admin?.isAdmin.valueOf();
 
   useEffect(() => {
     window.addEventListener("resize", handleResize);
@@ -67,21 +68,27 @@ const Navbar = ({ nameSportCenter }: PropsNavbar) => {
             </div>
             <div className="flex flex-col">
               <nav className="flex flex-col px-3 gap-4 pt-8">
-                <LinkNav
-                  href="/"
-                  iconPath="/icons/book"
-                  label="Reservaciones"
-                />
-                <LinkNav
-                  href="/servicios"
-                  iconPath="/icons/service_icon"
-                  label="Servicios"
-                />
-                <LinkNav
-                  href="/perfil"
-                  iconPath="/icons/profile"
-                  label="Perfil"
-                />
+                {!flag && (
+                  <LinkNav
+                    href="/"
+                    iconPath="/icons/book"
+                    label="Reservaciones"
+                  />
+                )}
+                {!flag && (
+                  <LinkNav
+                    href="/servicios"
+                    iconPath="/icons/service_icon"
+                    label="Servicios"
+                  />
+                )}
+                {!flag && (
+                  <LinkNav
+                    href="/perfil"
+                    iconPath="/icons/profile"
+                    label="Perfil"
+                  />
+                )}
                 {flag && (
                   <LinkNav
                     href="/access"
