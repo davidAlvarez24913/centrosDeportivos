c54277e136691e58f6fb7662d013b95ac7dabdb9
David Álvarez C <56882386+davidAlvarez24913@users.noreply.github.com>
2023-11-29 09:25:10 -0500

    update ip_server_public

--- packages/schema/src/gql/apolloClient.ts
+++ packages/schema/src/gql/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-const httpLink = new HttpLink({ uri: "http://159.89.85.206:4000/graphql" });
+const httpLink = new HttpLink({ uri: "http://159.65.176.238:4000/graphql" });
 export const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
